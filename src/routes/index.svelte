a5a3533a3136df60f094b38489fffe66c71dcd56
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-03-20 00:09:20 +0600

    localization

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 import SearchBar from '../components/SearchBar.svelte';	
 import Loader from '../components/Loader.svelte';
+import * as i18n from '../i18n';
+import { locale } from 'svelte-i18n';
+
+locale.set('ru');
 </script>
 
 <svelte:head>
