558bd42c0b5f4a25253755474c1bcf15e260ba7d
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-03-19 23:22:58 +0600

    Update styles

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -5,36 +5,16 @@
 	const dev = process.env.NODE_ENV === 'development';
 </script>
 
-<style>
-	h1, p {
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
 	<title>{status}</title>
 </svelte:head>
 
-<h1>{status}</h1>
+<div class="error-container">
+	<h1 class="error-title">{status}</h1>
 
-<p>{error.message}</p>
-
-{#if dev && error.stack}
-	<pre>{error.stack}</pre>
-{/if}
+	<p>{error.message}</p>
+	
+	{#if dev && error.stack}
+		<pre>{error.stack}</pre>
+	{/if}
+</div>
