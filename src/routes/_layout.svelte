83978d6c6dddd6cd9e335f7f88bfb2a6ebe4bc5f
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-03-26 14:39:03 +0600

    fix

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,18 +3,13 @@ import SearchBar from '../components/SearchBar.svelte';
 import MenuBar from '../components/MenuBar.svelte';
 import Loader from '../components/Loader.svelte';
 import { isLoading, locale } from 'svelte-i18n';
-import { get } from 'svelte/store';
 
 let menuBar: MenuBar;
 export let segment;
 
-$: if(!$isLoading) {
-	if(segment) {
-		if(get(locale) != segment) {
-			locale.set(segment);
-		}
-	}  
-}
+$: if(segment) {
+	locale.set(segment);
+} 
 </script>
 
 {#if $isLoading}
