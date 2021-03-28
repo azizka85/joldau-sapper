a06df03576c3f2b07af89d25af692e0b49003bd3
Aziz Kudaikulov <aziz.kudaikulov@gmail.com>
2021-03-28 12:32:55 +0600

    fix

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -52,7 +52,7 @@ function submit() {
       >
         <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
       </svg>			
-      <div class="search-bar-area">
+      <div class="search-bar-area" class:hide-search={settingsShowed}>
         <input type="text" placeholder={$_('search')} class="search-bar-area-input">
         <svg class="search-bar-area-icon" viewBox="0 0 16 16">
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
